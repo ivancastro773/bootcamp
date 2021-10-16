04b01339e96d1dff6b78f11eac1cd329a8850e95
chino <ivanchinocastro773@gmail.com>
2021-10-16 14:13:08 -0300

    agregue pagina web

---
+++ pagina web/js/sweetAlert.js
@@ -0,0 +1,53 @@
+function mensaje() {
+
+    swal({
+      title: 'Título',
+      text: 'Mensaje de texto',
+      html: '<p>Mensaje de texto con <strong>formato</strong>.</p>',
+      type: 'success',
+      timer: 3000,
+    });
+  }
+
+$('#formlogin').submit(function(e) {
+  e.preventDefault();
+  var name = $.trim($("#name").val());
+  var email = $.trim($("#email").val());
+  var province = $.trim($("#province").val());
+  var message = $.trim($("#message").val());
+    console.log(name);
+    console.log(email);
+    console.log(province);
+    console.log(message);
+    if (name.length == "" || email.length == "" || province.length == "" || message.length == "") {
+      Swal.fire({
+        title: 'Error!',
+        text: 'Campos vacíos, por favor complete todos los campos',
+        icon: 'error',
+        confirmButtonText: 'Ok'
+      })
+    }else{
+      Swal.fire({
+        title: 'Está seguro de enviar el email?',
+        text: "si desea cambiar los datos ingresados, presione cancelar.",
+        icon: 'warning',
+        showCancelButton: true,
+        cancelButtonText: 'Cancelar',
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sí,enviar!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Email enviado!',
+            'Pronto nos contactaremos con usted',
+            'success'
+          )
+        }
+      })
+    }
+ 
+    
+  
+  
+})
